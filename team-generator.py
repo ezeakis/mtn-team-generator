5d9f7da078f98c4a3e2b53b57639214c099481a8
ezeakis <ezeakis@gmail.com>
2022-01-05 22:05:01 +0200

    Create team-generator.py

---
+++ team-generator.py
@@ -0,0 +1,28 @@
+import random
+
+patients = [
+    "patient1", 
+    "patient2", 
+    "patient3", 
+    "patient4", 
+    "patient5", 
+    "patient6", 
+    "patient7", 
+    "patient8", 
+]
+
+teams = [
+    "team1", 
+    "team2", 
+    "team3", 
+    "team4", 
+]
+
+matrix = {}
+
+def random_team():
+    random.shuffle(teams)
+    return teams[0]
+
+for patient in patients:
+    print(patient, random_team())
